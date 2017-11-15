79b242ef41dbe0cb29786672c0b29932e2f0cbc9
sagacitysite <carlo.michaelis@gmail.com>
2017-11-15 12:08:28 +0100

    config added

---
+++ raspberry/config1.js
@@ -0,0 +1,10 @@
+module.exports = {
+    location_id: 1234,
+    latitude: '50.736',
+    longitude: '7.108',
+    temp_sensor_id: 11,
+    pm_sensor_id: 21,
+    privateKeyString: 'b6a5d1c2ce74a7aafa99511f7e556858e38a2ee6bd8c2e8f0196b86ae3d62a47',
+    //0xDAfFFd85f17180e52eef90B8eB0738d2AdD2b1A8
+    rpcAddress: 'http://192.168.1.10:8546'
+};
